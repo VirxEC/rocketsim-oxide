2aa03bf585a41c3731786cd44a90e461f1423db4
Virx <virx@virxcase.dev>
2025-07-28 12:36:06 -0400

    Reimplement bullet's `Quat` * `Quat` algo

--- src/bullet/linear_math/transform_util.rs
+++ src/bullet/linear_math/transform_util.rs
@@ -1,4 +1,4 @@
-use glam::{Affine3A, Mat3A, Quat, Vec3A};
+use glam::{Affine3A, Mat3A, Quat, Vec3A, Vec4, Vec4Swizzles};
 use std::f32::consts::FRAC_PI_4;
 
 const ANGULAR_MOTION_THRESHOLD: f32 = FRAC_PI_4;
@@ -12,6 +12,24 @@ pub fn integrate_transform_no_rot(
         matrix3: cur_trans.matrix3,
         translation: cur_trans.translation + lin_vel * time_step,
     }
+}
+
+/// A reimplementation of bullet's Quat * Quat algorithm,
+/// since using glam's results in some minute differences
+fn bullet_quat_mul(q1: Quat, q2: Quat) -> Quat {
+    const NEG_W: Vec4 = Vec4::new(1.0, 1.0, 1.0, -1.0);
+
+    let q1: Vec4 = q1.into();
+    let q2: Vec4 = q2.into();
+
+    let a2 = q1.yzxy() * q2.zxyy();
+    let a1 = q1.xyzx() * q2.wwwx() + a2;
+
+    let b1 = q1.zxyz() * q2.yzxz();
+    let a0 = q1.wwww() * q2 - b1;
+
+    let q = a0 + a1 * NEG_W;
+    Quat::from_array(q.to_array())
 }
 
 pub fn integrate_transform(
@@ -39,10 +57,11 @@ pub fn integrate_transform(
 
     let dorn = Quat::from_xyzw(axis.x, axis.y, axis.z, (angle * time_step * 0.5).cos());
     let orn0 = Quat::from_mat3a(&cur_trans.matrix3);
-    let predicted_orn = (dorn * orn0).normalize().conjugate();
+    let predicted_orn = bullet_quat_mul(dorn, orn0).normalize();
+    let matrix3 = Mat3A::from_quat(predicted_orn);
 
     Affine3A {
-        matrix3: Mat3A::from_quat(predicted_orn),
+        matrix3,
         translation,
     }
 }
