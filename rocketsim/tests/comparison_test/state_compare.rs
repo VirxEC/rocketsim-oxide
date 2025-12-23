31fd03b8fc80e4407bba1db3f546155647752f7b
ZealanL <36944229+ZealanL@users.noreply.github.com>
2025-12-23 14:01:22 -0800

    Add rest of car state to tests, make error threshold 1.0

--- rocketsim/tests/comparison_test/state_compare.rs
+++ rocketsim/tests/comparison_test/state_compare.rs
@@ -2,27 +2,24 @@ use glam::{Mat3A, Vec3A};
 use rocketsim::{BallState, CarState, PhysState};
 use rocketsim_rs::consts;
 
-fn calc_vec_err(a: Vec3A, b: Vec3A) -> f32 {
+fn calc_vec3_err(a: Vec3A, b: Vec3A) -> f32 {
     a.distance(b)
 }
 
 /// Outputs in range [0, 4] (assuming the rotation matrices are proper)
 fn calc_rot_err(a: Mat3A, b: Mat3A) -> f32 {
-    calc_vec_err(a.x_axis, b.x_axis)
-        + calc_vec_err(a.y_axis, b.y_axis)
-        + calc_vec_err(a.z_axis, b.z_axis)
+    calc_vec3_err(a.x_axis, b.x_axis)
+        + calc_vec3_err(a.y_axis, b.y_axis)
+        + calc_vec3_err(a.z_axis, b.z_axis)
 }
 
 const fn calc_bool_err(a: bool, b: bool) -> f32 {
     if a != b { 1.0 } else { 0.0 }
 }
 
-/// Outputs in range [0, 1] with diminishing importance as the time increases
+/// Every tick gives 0.5 error
 fn calc_time_err(a: f32, b: f32) -> f32 {
-    assert!(a >= 0.0 && b >= 0.0);
-    let frac_a = 1.0 / (1.0 + a);
-    let frac_b = 1.0 / (1.0 + b);
-    (frac_a - frac_b).abs()
+    (a - b).abs() * 60.0
 }
 
 ///////////////
@@ -30,7 +27,7 @@ fn calc_time_err(a: f32, b: f32) -> f32 {
 pub type StateErrSet = Vec<(String, f32)>;
 
 pub fn map_phys_err(err_set: &mut StateErrSet, a: &PhysState, b: &PhysState, include_rot: bool) {
-    err_set.push(("pos".to_string(), calc_vec_err(a.pos, b.pos) / 100.0));
+    err_set.push(("pos".to_string(), calc_vec3_err(a.pos, b.pos) / 100.0));
 
     if include_rot {
         err_set.push(("rot".to_string(), calc_rot_err(a.rot_mat, b.rot_mat) / 2.0));
@@ -38,10 +35,10 @@ pub fn map_phys_err(err_set: &mut StateErrSet, a: &PhysState, b: &PhysState, inc
 
     // NOTE: Vel is typically less important than pos, but tends to indicate the error earlier,
     //  so it gets a disproportionately-high priority.
-    err_set.push(("vel".to_string(), calc_vec_err(a.vel, b.vel) / 100.0));
+    err_set.push(("vel".to_string(), calc_vec3_err(a.vel, b.vel) / 100.0));
     err_set.push((
         "ang_vel".to_string(),
-        calc_vec_err(a.ang_vel, b.ang_vel) / 5.0,
+        calc_vec3_err(a.ang_vel, b.ang_vel) / 5.0,
     ));
 }
 
@@ -74,10 +71,12 @@ pub fn map_car_err(a: &CarState, b: &CarState) -> StateErrSet {
         "boost".to_string(),
         (a.boost - b.boost).abs() / consts::BOOST_MAX,
     ));
+
     err_set.push((
         "is_on_ground".to_string(),
         calc_bool_err(a.is_on_ground, b.is_on_ground),
     ));
+
     err_set.push((
         "is_jumping".to_string(),
         calc_bool_err(a.is_jumping, b.is_jumping),
@@ -86,6 +85,7 @@ pub fn map_car_err(a: &CarState, b: &CarState) -> StateErrSet {
         "has_jumped".to_string(),
         calc_bool_err(a.has_jumped, b.has_jumped),
     ));
+
     err_set.push((
         "is_flipping".to_string(),
         calc_bool_err(a.is_flipping, b.is_flipping),
@@ -128,6 +128,66 @@ pub fn map_car_err(a: &CarState, b: &CarState) -> StateErrSet {
     err_set.push((
         "wheel_3_contact".to_string(),
         calc_bool_err(a.wheels_with_contact[3], b.wheels_with_contact[3]),
+    ));
+
+    err_set.push((
+        "flip_rel_torque".to_string(),
+        calc_vec3_err(a.flip_rel_torque, b.flip_rel_torque),
+    ));
+
+    err_set.push((
+        "jump_time".to_string(),
+        calc_time_err(a.jump_time, b.jump_time),
+    ));
+
+    err_set.push((
+        "flip_time".to_string(),
+        calc_time_err(a.flip_time, b.flip_time),
+    ));
+
+    err_set.push((
+        "time_since_boosted".to_string(),
+        calc_time_err(a.time_since_boosted, b.time_since_boosted),
+    ));
+
+    err_set.push((
+        "is_boosting".to_string(),
+        calc_bool_err(a.is_boosting, b.is_boosting),
+    ));
+
+    err_set.push((
+        "boosting_time".to_string(),
+        calc_time_err(a.boosting_time, b.boosting_time),
+    ));
+
+    err_set.push((
+        "is_supersonic".to_string(),
+        calc_bool_err(a.is_supersonic, b.is_supersonic),
+    ));
+
+    err_set.push((
+        "supersonic_time".to_string(),
+        calc_time_err(a.supersonic_time, b.supersonic_time),
+    ));
+
+    err_set.push((
+        "handbrake_val".to_string(),
+        (a.handbrake_val - b.handbrake_val).abs(),
+    ));
+
+    err_set.push((
+        "is_auto_flipping".to_string(),
+        calc_bool_err(a.is_auto_flipping, b.is_auto_flipping),
+    ));
+
+    err_set.push((
+        "auto_flip_timer".to_string(),
+        calc_time_err(a.auto_flip_timer, b.auto_flip_timer),
+    ));
+
+    err_set.push((
+        "auto_flip_torque_scale".to_string(),
+        (a.auto_flip_torque_scale - b.auto_flip_torque_scale).abs(),
     ));
 
     for (name, _) in err_set.iter_mut() {
