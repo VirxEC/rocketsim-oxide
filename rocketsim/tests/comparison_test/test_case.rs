1c550216a0f25caeae6f2ce3b97f9f9b7a42c43e
ZealanL <36944229+ZealanL@users.noreply.github.com>
2025-12-29 02:53:48 -0800

    Fix old cars in tests not having proper control updates

--- rocketsim/tests/comparison_test/test_case.rs
+++ rocketsim/tests/comparison_test/test_case.rs
@@ -152,9 +152,18 @@ impl TestCase {
                 let car_id_old = (i + 1) as u32;
                 let new_car_state = *new_arena.car(car_id_new).get_state();
                 let old_car_state = old_arena_ptr.pin_mut().get_car(car_id_old);
-                let old_car_state_conv =
+
+                // Set old car's controls to new car's controls
+                old_arena_ptr.pin_mut().set_car_controls(
+                    car_id_old,
+                    state_convert::conv_to_old_car_controls(controls),
+                );
+
+                // Update new arena car to old arena car's state
+                let mut old_car_state_conv =
                     state_convert::conv_to_new_car_state(&old_car_state, controls);
                 new_arena.set_car_state(car_id_new, old_car_state_conv);
+
                 car_state_pairs.push((new_car_state, old_car_state_conv));
             }
 
